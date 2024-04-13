c0fe8785edc77f3571123ff9adc3983de8d20177
jianyaoli <nuc2015@163.com>
2024-04-13 22:30:08 +0800

    007-生命周期

---
+++ 过程备份/007-生命周期/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>APP</h2>
+  <Person v-if="isShow"/>
+</template>
+
+<script lang="ts" setup>
+  import Person from './components/Person.vue'
+  import {ref,onMounted} from 'vue'
+
+  let isShow = ref(true)
+
+  // 挂载完毕
+  onMounted(()=>{
+    console.log('父---挂载完毕')
+  })
+</script>
+
+<style scoped>
+  hr {
+    border: 2px red dashed;
+  }
+</style>
