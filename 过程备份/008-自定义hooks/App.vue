8992a80e524d1b27bb9b9650bd99f3db49c6bfc2
jianyaoli <nuc2015@163.com>
2024-04-13 23:25:28 +0800

    009-自定义Hooks

---
+++ 过程备份/008-自定义hooks/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>APP</h2>
+  <Person/>
+</template>
+
+<script lang="ts" setup>
+  import Person from './components/Person.vue'
+</script>
+
+<style scoped>
+  hr {
+    border: 2px red dashed;
+  }
+</style>
