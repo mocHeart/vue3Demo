845ec4a794e6615e0c12f7ccfa0d7612fc95f77c
jianyaoli <nuc2015@163.com>
2024-04-20 23:02:44 +0800

    011-编程式路由

---
+++ 过程备份/011-编程式路由/views/Detail.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- Query参数 -->
+  <!-- <ul class="news-list">
+    <li>编号：{{ query.id }}</li>
+    <li>标题：{{ query.title }}</li>
+    <li>内容：{{ query.content }}</li>
+  </ul> -->
+
+
+  <!-- Params参数 -->
+  <!-- <ul class="news-list">
+    <li>编号：{{ params.id }}</li>
+    <li>标题：{{ params.title }}</li>
+    <li>内容：{{ params.content }}</li>
+  </ul> -->
+
+  <!-- 路由的Props写法 -->
+  <ul class="news-list">
+    <li>编号：{{id}}</li>
+    <li>标题：{{title}}</li>
+    <li>内容：{{content}}</li>
+  </ul>
+</template>
+
+<script setup lang="ts" name="About">
+  import {toRefs} from 'vue'
+  import {useRoute} from 'vue-router'
+
+  // 路由的Props写法
+  defineProps(['id','title','content'])
+
+  let route = useRoute()
+  let {query} = toRefs(route)
+  let {params} = toRefs(route)
+
+
+</script>
+
+<style scoped>
+  .news-list {
+    list-style: none;
+    padding-left: 20px;
+  }
+
+  .news-list>li {
+    line-height: 30px;
+  }
+</style>
