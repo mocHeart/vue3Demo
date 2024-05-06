f189d7099b2cf7ffadd57792ff7d95170368397f
jianyaoli <nuc2015@163.com>
2024-05-06 22:18:35 +0800

    013-组件通信的9种方式

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// 第一步：引入Pinia
 import {createPinia} from 'pinia'
+import router from './router'
 
 const app = createApp(App)
-// 第二步：创建Pinia
 const pinia = createPinia()
-// 第三步：安装Pinia
+
+
 app.use(pinia)
+app.use(router)
 app.mount('#app')
